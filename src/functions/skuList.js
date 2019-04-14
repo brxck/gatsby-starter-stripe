b04eefe87042d3530d620d6529034cccd43ffdfe
Brock McElroy <28877984+brxck@users.noreply.github.com>
2019-04-13 17:10:24 -0700

    Refactor

--- src/functions/skuList.js
+++ src/functions/skuList.js
@@ -1,5 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
+/**
+ * Returns list of skus with product fields expanded.
+ */
 module.exports.handler = (event, context, callback) => {
   stripe.skus.list(
     {
