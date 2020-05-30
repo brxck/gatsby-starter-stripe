3289427fb1383183af9efc041f17201e2f3037e5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-05-30 08:50:34 -0700

    Utilize Netlify Dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-})
-
 module.exports = {
   siteMetadata: {
     title: `starter-stripe`,
     description: `Stripe storefront starter for Gatsby`,
-    author: `Brock McElroy <brxck.dev>`
+    author: `Brock McElroy <brxck.dev>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,8 +10,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -30,30 +24,17 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
     },
     {
       resolve: `gatsby-source-stripe`,
       options: {
-        objects: ['Product', 'Sku'],
+        objects: ["Product", "Sku"],
         secretKey: process.env.STRIPE_SECRET_KEY,
         downloadFiles: true,
-        auth: false
-      }
-    }
+        auth: false,
+      },
+    },
   ],
-  // for avoiding CORS while developing Netlify Functions locally
-  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
-    app.use(
-      '/.netlify/functions/',
-      createProxyMiddleware({
-        target: 'http://localhost:9000',
-        pathRewrite: {
-          '/.netlify/functions/': ''
-        }
-      })
-    )
-  }
 }
