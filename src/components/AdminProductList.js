c1057868f423937a7c3fe8f48c32eab726924d26
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-06-07 11:17:04 -0700

    Create product table

--- src/components/AdminProductList.js
+++ src/components/AdminProductList.js
@@ -9,13 +9,55 @@ export const AdminProductForm = ({ productId }) => {
   const products = listProducts()
 
   return (
-    <ul>
-      {products.map(({ id, name }) => (
-        <Link to={`/admin/product/${id}`} key={id}>
-          <li>{name}</li>
-        </Link>
-      ))}
-    </ul>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Inventory</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(({ id, name, skus, active }) => (
+            <>
+              <tr key={id}>
+                <td>
+                  <input type="checkbox" checked={active} readOnly />{" "}
+                  <Link to={`/admin/product/${id}`}>{name}</Link>
+                </td>
+                {skus.length === 1 && (
+                  <>
+                    <td>${(skus[0].price / 100).toFixed(2)}</td>
+                    <td>
+                      {skus[0].inventory.value ??
+                        skus[0].inventory.quantity ??
+                        "∞"}
+                    </td>
+                  </>
+                )}
+              </tr>
+              {skus.length > 1 &&
+                skus.map(({ id, price, inventory, attributes }) => (
+                  <tr key={id}>
+                    <td>
+                      <span style={{ marginLeft: "2rem" }}>
+                        {attributes.name}
+                      </span>
+                    </td>
+                    <td>${(price / 100).toFixed(2)}</td>
+                    <td>
+                      {skus[0].inventory.value ??
+                        skus[0].inventory.quantity ??
+                        "∞"}
+                    </td>
+                  </tr>
+                ))}
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 AdminProductForm.propTypes = {
