0b0eeb5e64d26c4ffd5f2aee35ddd7e2428556c5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-06-23 08:29:51 -0700

    Handle product deletion

---
+++ functions/productDelete.js
@@ -0,0 +1,18 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+module.exports.handler = async (event, context, callback) => {
+  const requestBody = JSON.parse(event.body)
+  // All skus must be deleted before product
+  const skus = await stripe.skus.list({ product: requestBody.productId })
+  await Promise.all(skus.data.map(({ id }) => stripe.skus.del(id)))
+  await stripe.products.del(requestBody.productId)
+
+  const response = {
+    statusCode: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "b*",
+    },
+    body: "",
+  }
+  callback(null, response)
+}
