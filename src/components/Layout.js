ad97e330a9fd002580807a879c00a1b931ef198f
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-06-28 15:06:20 -0700

    Migrate static queries to hooks

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,53 +1,49 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import Header from "./Header"
 import ProductsProvider from "./ProductsProvider"
 import CartProvider from "./CartProvider"
 import IdentityProvider from "./IdentityProvider"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <>
-        <IdentityProvider>
-          <ProductsProvider>
-            <CartProvider>
-              <Header title={data.site.siteMetadata.title} />
-              <main
-                style={{
-                  margin: `0 auto`,
-                  maxWidth: 960,
-                  padding: `1.5rem 0`,
-                }}
-              >
-                {children}
-              </main>
-              <footer style={{ textAlign: "center", margin: "3rem 0" }}>
-                Check out
-                {` `}
-                <a href="https://github.com/brxck/gatsby-starter-stripe">
-                  gatsby-starter-stripe
-                </a>
-                {` `}
-                on GitHub!
-              </footer>
-            </CartProvider>
-          </ProductsProvider>
-        </IdentityProvider>
-      </>
-    )}
-  />
-)
+    }
+  `)
+  return (
+    <IdentityProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <Header title={data.site.siteMetadata.title} />
+          <main
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `1.5rem 0`,
+            }}
+          >
+            {children}
+          </main>
+          <footer style={{ textAlign: "center", margin: "3rem 0" }}>
+            Check out
+            {` `}
+            <a href="https://github.com/brxck/gatsby-starter-stripe">
+              gatsby-starter-stripe
+            </a>
+            {` `}
+            on GitHub!
+          </footer>
+        </CartProvider>
+      </ProductsProvider>
+    </IdentityProvider>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
