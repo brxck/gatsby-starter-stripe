1d80480f9da14de393077414e46eaa9762b54da7
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-08-17 11:14:36 -0700

    Remove unused identity provider

--- src/components/StoreLayout.js
+++ src/components/StoreLayout.js
@@ -4,49 +4,46 @@ import PropTypes from "prop-types"
 import Header from "./Header"
 import ProductsProvider from "./ProductsProvider"
 import CartProvider from "./CartProvider"
-import IdentityProvider from "./IdentityProvider"
 import css from "./StoreLayout.module.css"
 
 const StoreLayout = ({ children }) => {
   return (
-    <IdentityProvider>
-      <ProductsProvider>
-        <CartProvider>
-          <Header cart></Header>
+    <ProductsProvider>
+      <CartProvider>
+        <Header cart></Header>
 
-          <main className={css.main}>{children}</main>
+        <main className={css.main}>{children}</main>
 
-          <footer style={{ textAlign: "center", margin: "2rem 0" }}>
-            <span>
-              Fork
-              {` `}
-              <a href="https://github.com/brxck/gatsby-starter-stripe">
-                gatsby-starter-stripe
-              </a>
-              {` `}
-              on GitHub!{" "}
-            </span>
+        <footer style={{ textAlign: "center", margin: "2rem 0" }}>
+          <span>
+            Fork
+            {` `}
+            <a href="https://github.com/brxck/gatsby-starter-stripe">
+              gatsby-starter-stripe
+            </a>
+            {` `}
+            on GitHub!{" "}
+          </span>
 
-            <div className={css.footerIcons}>
-              <a href="https://github.com/brxck/gatsby-starter-stripe">
-                <img
-                  height="32"
-                  width="32"
-                  src="https://unpkg.com/simple-icons@v2/icons/github.svg"
-                />
-              </a>
-              <a href="https://www.gatsbyjs.org/starters/brxck/gatsby-starter-stripe/">
-                <img
-                  height="32"
-                  width="32"
-                  src="https://unpkg.com/simple-icons@v2/icons/gatsby.svg"
-                />
-              </a>
-            </div>
-          </footer>
-        </CartProvider>
-      </ProductsProvider>
-    </IdentityProvider>
+          <div className={css.footerIcons}>
+            <a href="https://github.com/brxck/gatsby-starter-stripe">
+              <img
+                height="32"
+                width="32"
+                src="https://unpkg.com/simple-icons@v2/icons/github.svg"
+              />
+            </a>
+            <a href="https://www.gatsbyjs.org/starters/brxck/gatsby-starter-stripe/">
+              <img
+                height="32"
+                width="32"
+                src="https://unpkg.com/simple-icons@v2/icons/gatsby.svg"
+              />
+            </a>
+          </div>
+        </footer>
+      </CartProvider>
+    </ProductsProvider>
   )
 }
 
